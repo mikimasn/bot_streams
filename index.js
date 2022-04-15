6473fff98149ee7c00112eceb954b276c56cedc4
pythontest <50592309+mikimasn@users.noreply.github.com>
2022-04-15 16:20:05 +0200

    working bot

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const discord = require("discord.js")
 const secret = require("./secret.json")
-var client = new discord.Client({intents:[discord.Intents.FLAGS.GUILDS]});
+var client = new discord.Client({intents:[discord.Intents.FLAGS.GUILDS,discord.Intents.FLAGS.GUILD_MESSAGES]});
 const functions = require("./functions")
+const db = require("./db")
+const dbc = new db();
+const config = require("./config.json")
 client.on("ready",()=>{
     console.log("ready");
 });
-client.on("message",()=>{
-
+client.on("messageCreate",(message)=>{
+    var uid = message.author.id
+    dbc.readlogs(uid,Math.floor(new Date()/1000-config.timeout),rows=>{
+        if(rows.length==0){
+            dbc.addxp(uid,functions.randomint(config.minxp,config.maxxp),{type:0,msg:message.id});
+        }
+    })
+})
+client.on("interactionCreate",inter=>{
+    if(inter.isCommand()){
+        
+    }
 })
 client.login(secret.token)
